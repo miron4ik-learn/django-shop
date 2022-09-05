38e898207dc52bc5fa1b35763b63ac1c87fa4d29
miron4ik <miron4ik.life@yandex.ru>
2022-09-05 15:12:16 +0300

    Show product image in admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from . import models
 
 @admin.register(models.Category)
@@ -8,7 +10,13 @@ class CategoryAdmin(admin.ModelAdmin):
   
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-  list_display = [ 'name', 'slug', 'price', 'available', 'created_at', 'updated_at' ]
+  list_display = [ 'name', 'slug', 'image_show', 'price', 'available', 'created_at', 'updated_at' ]
   list_filter = [ 'available', 'created_at', 'updated_at' ]
   list_editable = [ 'price', 'available' ]
   prepopulated_fields = { 'slug': ('name',) }
+  
+  def image_show(self, obj):
+    if obj.image:
+      return mark_safe('<img src="{}" width="30">'.format(obj.image.url))
+    return 'None'
+  image_show.__name__ = 'Картинка'
