1705951e14c34d5474bc598f49ecb003257ad1ee
miron4ik <miron4ik.life@yandex.ru>
2022-09-05 14:34:38 +0300

    Create models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = [ 'name', 'slug' ]
+  prepopulated_fields = { 'slug': ('name',) }
+  
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+  list_display = [ 'name', 'slug', 'price', 'available', 'created_at', 'updated_at' ]
+  list_filter = [ 'available', 'created_at', 'updated_at' ]
+  list_editable = [ 'price', 'available' ]
+  prepopulated_fields = { 'slug': ('name',) }
--- main/models.py
+++ main/models.py
@@ -1,2 +1,36 @@
 from django.db import models
 
+class Category(models.Model):
+  class Meta:
+    ordering = ('name',)
+    verbose_name = 'Категория',
+    verbose_name_plural = 'Категории'
+  
+  name = models.CharField(verbose_name='Название', max_length=100, db_index=True)
+  slug = models.SlugField(max_length=100, unique=True)
+  
+  def __str__(self):
+    return self.name
+  
+class Product(models.Model):
+  class Meta:
+    ordering = ('name',)
+    verbose_name = 'Товар',
+    verbose_name_plural = 'Товары'
+    index_together = (('id', 'slug'),)
+    
+  category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+  
+  name = models.CharField(verbose_name='Название', max_length=150, db_index=True)
+  slug = models.SlugField(max_length=150, db_index=True, unique=True)
+  
+  image = models.ImageField(verbose_name='Картинка', upload_to='product/%Y/%m/%d', blank=True)
+  description = models.TextField(verbose_name='Описание', max_length=1000, blank=True)
+  price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+  available = models.BooleanField(verbose_name='Наличие', default=True)
+  
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return self.name
